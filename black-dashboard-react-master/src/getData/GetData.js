f215dc5e67ca04cc474452e021b6b4bc0fd8aaaa
Shane <shanemonck11@hotmail.com>
2021-08-27 01:17:21 +0800

    fetches db and pitch from room 1 sensor 0

--- black-dashboard-react-master/src/getData/GetData.js
+++ black-dashboard-react-master/src/getData/GetData.js
@@ -2,22 +2,22 @@ import React, {useState, useEffect} from "react"
 import axios from 'axios';
 
 const GetData = () => {
-    const [fechtedData, setFechtedData] = useState([]);
+    const [fetchedData, setFetchedData] = useState([]);
     
     useEffect( () => { 
         async function fetchData() {
             try {
-                const res = await axios.get('http://127.0.0.1:8000/'); 
-                setFechtedData(res.data);
-               
-            } catch (err) {
+                const res = await axios.get('http://127.0.0.1:8001/1/0'); 
+                setFetchedData(res.data);
                 
+            } catch (err) {
+                console.log("ERRRORROROROR");
                 console.log(err);
             }
         }
         fetchData();
     }, []);
-    return <div>{fechtedData}</div>
+    return <div>Db is = {fetchedData.dB} pitch = {fetchedData.pitch}</div>
 }
 
 export default GetData
